8fd490d539449d904caec7fd5982709120922aea
Marcus Willock <crazcalm@gmail.com>
2013-10-29 16:00:35 -0400

    Not perfect, but the basic structure is there

--- movies_info.py
+++ movies_info.py
@@ -15,14 +15,33 @@ def scrape_info(html):
     """
     Will scrape the movie data that I want.
     """
+    # Will be a list of dicts (movie info)
+    all_info = []
+    
     for item in html:
         item = str(item)
         
-        movie_name(item)
-        movie_poster(item)
-        movie_duration(item)
-        movie_categories(item)
-        movie_showtimes(item)
+        # Will store the info for individual movies.
+        movie_info = {}
+        
+        name              = movie_name(item)
+        poster_and_rating = movie_poster(item)
+        duration          = movie_duration(item)
+        categories        = movie_categories(item)
+        showtimes         = movie_showtimes(item)
+        
+        movie_info["name"] = name
+        movie_info["poster_and_rating"] = poster_and_rating
+        movie_info["duration"] = duration
+        movie_info["categories"] = categories
+        movie_info["showtimes"] = showtimes
+        
+        all_info.append(movie_info)
+        
+    for item in all_info:
+        print "\n"
+        for key in item:
+            print key, item[key]
     """
     find_title = re.compile('itemprop="url">(.*)</a>')
     theater_title = re.findall(find_title, str(theater_html1))
@@ -31,20 +50,34 @@ def scrape_info(html):
 def movie_showtimes(html):
     """
     Scrapes movie showtimes for regular, 3D and Imax
+    
+    issue: It only scrapes the first set of movie times...
     """
     
     soup = BeautifulSoup(html)
     soup = soup.find_all("div", {"class": "showtimes"})
     
-    print "\n"
+    #print "\n", "len(soup)", len(soup)
     
     for stuff in soup:
-        #print stuff
         
-        times = re.compile('data-displaytimes="(.*)" data-logo-url')
-        times = re.findall(times, str(stuff))
-        
+        times = showtimes(stuff)
+        """
+        if len(soup) > 1:
+            print soup[1]
+            print "\n", times
+        """
         #print times
+        return times
+
+def showtimes(html):
+    """
+    parses movie times
+    """
+    times = re.compile('data-displaytimes="(.*)" data-logo-url')
+    times = re.findall(times, str(html))
+    
+    return times
 
 def movie_categories(html):
     """
@@ -61,6 +94,9 @@ def movie_categories(html):
     soup = BeautifulSoup(html)
     soup = soup.find("p", {"class":"cert-runtime-genre"})
     
+    # will hold the movie times
+    stack = []
+    
     #print soup
     #print soup.strings
     
@@ -72,10 +108,25 @@ def movie_categories(html):
         for item in string:
             try:
                 if item[0].isalpha():
-                    #print item
-                    pass
+                    
+                    item = kill_whitespace(item)
+                    #print "category", item, "\n", "length", len(item)
+                    
+                    stack.append(item)
             except:
                 pass
+    
+    return stack
+    
+def kill_whitespace(text):
+    """
+    Removes the whitespace that is on the right side of the text.
+    """
+    
+    while text[-1] == " ":
+        text = text[:-1]
+        
+    return text
             
 def movie_duration(html):
     """
@@ -86,6 +137,7 @@ def movie_duration(html):
     duration = re.findall(duration, html)
     
     #print duration
+    return duration
 
 def movie_poster(html):
     """
@@ -95,6 +147,7 @@ def movie_poster(html):
     poster = re.findall(poster, html)
     
     #print poster
+    return poster
         
 
 def movie_name(html):
@@ -106,7 +159,7 @@ def movie_name(html):
     name = name[0][:-6]
     
     #print "name:", name, "\n"
-    
+    return name
     
 
 
